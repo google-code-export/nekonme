16259c711baf55c0e31beac1c4dc3e8167d30fb5
Hugh <gamehaxe@gmail.com>
2013-11-05 14:44:59 +0800

    Add png + jpeg

--- build/libs/Build.hx
+++ build/libs/Build.hx
@@ -6,9 +6,15 @@ class Build
 
    public static function main()
    {
+      //buildArgs.push("-Dandroid");
+
       mkdir("bin");
       mkdir("unpack");
+      mkdir("include");
+
       buildZ("1.2.3");
+      buildPng("1.2.24");
+      buildJpeg("6b");
    }
 
    public static function mkdir(inDir:String)
@@ -44,8 +50,38 @@ class Build
    public static function buildZ(inVer:String)
    {
       untar("zlib-" + inVer + ".tgz");
-      copy("buildfiles/zlib.xml", 'unpack/zlib-$inVer');
-      runIn('unpack/zlib-$inVer', "haxelib", ["run", "hxcpp", "zlib.xml"].concat(buildArgs));
+      var dir = 'unpack/zlib-$inVer';
+      copy("buildfiles/zlib.xml", dir);
+      runIn(dir, "haxelib", ["run", "hxcpp", "zlib.xml"].concat(buildArgs));
+      copy('$dir/zlib.h',"include");
+      copy('$dir/zconf.h',"include");
    }
+
+   public static function buildPng(inVer:String)
+   {
+      untar("libpng-" + inVer + ".tgz");
+      var dir = 'unpack/libpng-$inVer';
+      copy("buildfiles/png.xml", dir);
+      runIn(dir, "haxelib", ["run", "hxcpp", "png.xml"].concat(buildArgs));
+      copy('$dir/png.h',"include");
+      copy('$dir/pngconf.h',"include");
+   }
+
+   public static function buildJpeg(inVer:String)
+   {
+      untar("jpeg-" + inVer + ".tgz");
+      var dir = 'unpack/jpeg-$inVer';
+      var configs = [ "h", "mac", "iphoneos", "vc", "linux" ];
+      for(config in configs)
+         copy('configs/jconfig.$config', dir);
+
+      copy("buildfiles/jpeg.xml", dir);
+      runIn(dir, "haxelib", ["run", "hxcpp", "jpeg.xml"].concat(buildArgs));
+      for(config in configs)
+         copy('configs/jconfig.$config', "include");
+      copy('$dir/jpeglib.h',"include");
+      copy('$dir/jmorecfg.h',"include");
+   }
+
 }
 
